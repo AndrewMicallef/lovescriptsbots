dcdf6999804f740f5a06705923390161c539e348
Andrew <andrew.micallef@live.com.au>
2020-05-09 20:38:13 +1000

    wip refactor neurons

--- src/NeuronPrimary.lua
+++ src/NeuronPrimary.lua
@@ -12,27 +12,41 @@ function NeuronPrimary:init(def)
     self.inputs = def.inputs
     self.outputs = def.outputs
 
-    -- synapse types: 'sensor', 'motor', 'inter'
-    -- inter neurons are randomly connecteted to any neuron in the brain
-    -- sensor neurons recieve input direct from a property of agent
-    -- motor neurons output direct to a property of agent
-    for i=1, CONNECTIONS do
-        local synapse_id
-        synapse_id = math.random(1, BRAINSIZE)
-        -- make at least 20% of neurons link to input sensors
-        if (math.random() < 0.2) then
-            synapse_id = math.random(1,def.inputsize)
-        end
-
-        self.synapse[i] = {
-            w = randf(0.1, 2), -- input weights
-            synapse_id = synapse_id, -- id of synapse
-            is_inhibitory = math.random(0,1) -- T/F inhibition
-        }
-    end
-
     self.output = 0 -- current output
     self.target = 0 -- target output state
 
-    self.insource =
+    self.synapses = {}
+    --self.insource =
+end
+
+function NeuronPrimary:setSynapses()
+    -- synapse types: 'sensor', 'motor', 'inter'
+    -- inter neurons are randomly connected to any neuron in the brain
+    -- sensor neurons recieve input direct from a property of agent
+    -- motor neurons output direct to a property of agent
+    for i, input in ipairs(self.inputs) do
+
+        local synapse = Synapse()
+
+        table.insert(self.synapses, synapse)
+        
+    end
+end
+
+
+Synapse = Class{}
+
+function Synapse:init(def)
+    -- number between 0 - 2
+    self.weight = def.weight
+
+    -- string; one of 'excitatory' or 'inhibitory'
+    self.transmitter = def.transmitter
+
+    self.pre = def.pre
+    self.post = def.post
+
+    -- allows for transduction function in the case of sensors / actuators
+    self.pre_func = def.pre_func
+    self.post_func = def.post_func
 end
