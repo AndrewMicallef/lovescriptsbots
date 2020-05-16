58decc87a0d271f40635724a72e75a680cd4e1a0
Andrew Micallef <andrew.micallef@live.com.au>
2020-05-17 09:43:32 +1000

    fixes requires

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -1,18 +1,16 @@
-Class = require 'lib/hump/class'
+Class = require 'lib.hump.class'
+Vector = require 'lib.hump.vector'
 
-Vector = require 'lib/hump/vector'
+require 'src.utility'
 
-require 'src/utility'
-
-require 'src/constants'
+require 'src.constants'
 
 --building blocks
 
-require 'src/geometry/vertex'
-require 'src/geometry/PolygonBody'
+require 'src.geometry.Vertex'
+require 'src.geometry.PolygonBody'
 
+require 'src.Dummy'
 
-require 'src/Dummy'
-
-require 'src/brain'
-require 'src/agent'
+require 'src.Brain'
+require 'src.Agent'
