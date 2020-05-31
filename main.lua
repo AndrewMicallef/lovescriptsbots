2c0214a459d3991085609afbf0b826ecf4e78579
Andrew Micallef <andrew.micallef@live.com.au>
2020-05-31 13:00:43 +1000

    basic molecule base

--- main.lua
+++ main.lua
@@ -2,12 +2,29 @@ require "src.Dependancies"
 
 function love.load()
 
+    WIDTH, HEIGHT = love.window.getMode()
     gWorld = love.physics.newWorld()
+
+    gEntities = {
+        ['molecule'] = Molecule{world=gWorld,
+                             loc = Vector(WIDTH/2, HEIGHT/2)
+                             }
+    }
+
+
 
 end
 
 function love.update(dt)
+
+    for k, entity in pairs(gEntities) do
+        entity:update(dt)
+    end
 end
 
 function love.draw()
+
+    for k, entity in pairs(gEntities) do
+        entity:render()
+    end
 end
--- src/Molecule.lua
+++ src/Molecule.lua
@@ -6,17 +6,29 @@ function Molecule:init(props)
 
     self.loc = props.loc or Vector.zero
     self.weight = props.weight or 1
-    self.radius = props.radius or 1
+    self.radius = props.radius or 25
 
-    self.statemachine = props.statemachine or StateMachine()
-    self.bindsites = props.bindsites
+    self.world = props.world
+    self.body = love.physics.newBody(self.world, self.loc.x, self.loc.y, 'dynamic')
+    self.shape = love.physics.newCircleShape(self.radius)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+
+    --self.statemachine = props.statemachine or StateMachine()
+    self.bindsites = props.bindsites or {}
 end
 
 function Molecule:update(dt)
     -- arrange bindsites in a circular pattern at the edge of the molecule
-    self.statemachine:update(dt)
+    --self.statemachine:update(dt)
 end
 
 function Molecule:render()
-    self.statemachine:render()
+    --self.statemachine:render()
+
+    love.graphics.setColor(gColor['white'])
+    love.graphics.circle('line', self.loc.x, self.loc.y, self.radius)
+
+    for k, site in pairs(self.bindsites) do
+        site:render()
+    end
 end
